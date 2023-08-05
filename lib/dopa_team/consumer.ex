b8a1da4ee6e1c6aaaaae56578a800947bc0cf6e0
Ellyse <ellyxir>
2023-08-05 23:05:38 +0000

    grab the role ids for 18plus and minors

--- lib/dopa_team/consumer.ex
+++ lib/dopa_team/consumer.ex
@@ -2,11 +2,13 @@ defmodule DopaTeam.Consumer do
   use Nostrum.Consumer
 
   alias Nostrum.Api
-#  require Logger
+  require Logger
 
   @sdr_channel 849_267_950_988_820_482
   @bot_test_dm_asking 964_397_371_666_604_079
   @channels [@sdr_channel, @bot_test_dm_asking]
+  @adult_role_name "18+"
+  @minor_role_name "<18"
 
   def handle_event(
         {:MESSAGE_CREATE,
@@ -30,10 +32,15 @@ defmodule DopaTeam.Consumer do
             "roles for mentioned user #{mentioned_user_id}, roles=#{inspect(mentioned_user_roles)}\n"
         end)
 
+      server_roles = get_server_roles(guild_id)
+      {:ok, adult_role_id} = get_role_id_by_name(server_roles, @adult_role_name)
+      {:ok, minor_role_id} = get_role_id_by_name(server_roles, @minor_role_name)
+
       Api.create_message(
         channel_id,
         "message has mentions, list of user ids mentioned=#{inspect(mentioned_users)}\n" <>
           "author roles=#{inspect(author_roles)}\n" <>
+          "adult role id=#{inspect(adult_role_id)}\nminor role id=#{inspect(minor_role_id)}\n" <>
           mentioned_string
       )
     end
@@ -44,6 +51,31 @@ defmodule DopaTeam.Consumer do
     {:ok, %Nostrum.Struct.Guild.Member{roles: roles}} =
       Nostrum.Api.get_guild_member(guild_id, user_id)
 
+    roles
+  end
+
+  @spec get_role_id_by_name([Nostrum.Struct.Guild.Role.t()], String.t()) ::
+          {:ok, Nostrum.Struct.Guild.Role.t()} | {:error, String.t()}
+  def get_role_id_by_name(server_roles, role_name)
+      when is_list(server_roles) and is_binary(role_name) do
+    role_id =
+      Enum.find_value(server_roles, fn elem ->
+        if elem.name == role_name do
+          elem.id
+        else
+          false
+        end
+      end)
+
+    case role_id do
+      nil -> {:error, "role id not found"}
+      id when is_number(id) -> {:ok, id}
+    end
+  end
+
+  @spec get_server_roles(Nostrum.Struct.Guild.id()) :: [Nostrum.Struct.Guild.Role.t()]
+  def get_server_roles(guild_id) when is_integer(guild_id) do
+    {:ok, roles} = Nostrum.Api.get_guild_roles(guild_id)
     roles
   end
 
