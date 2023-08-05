4a1ac85a1abf06189402257b07fe5779cf7936e3
Ellyse <ellyxir>
2023-08-05 22:31:46 +0000

    gather roles for all mentioned users

--- lib/dopa_team/consumer.ex
+++ lib/dopa_team/consumer.ex
@@ -2,29 +2,51 @@ defmodule DopaTeam.Consumer do
   use Nostrum.Consumer
 
   alias Nostrum.Api
-  # require Logger
+#  require Logger
 
   @sdr_channel 849_267_950_988_820_482
   @bot_test_dm_asking 964_397_371_666_604_079
   @channels [@sdr_channel, @bot_test_dm_asking]
 
   def handle_event(
-        {:MESSAGE_CREATE, %Nostrum.Struct.Message{author: %{bot: is_bot}, channel_id: channel_id, member: %{roles: author_roles}, content: content} = msg, _ws_state}
+        {:MESSAGE_CREATE,
+         %Nostrum.Struct.Message{
+           author: %{bot: is_bot},
+           guild_id: guild_id,
+           channel_id: channel_id,
+           member: %{roles: author_roles},
+           content: content
+         }, _ws_state}
       )
       when channel_id in @channels and is_nil(is_bot) do
     if has_mentions?(content) do
-        Api.create_message(
-          channel_id,
-          "message has mentions, list of user ids mentioned=#{inspect get_mentions(content)}, author roles=#{inspect author_roles}"
-        )
+      mentioned_users = get_mentions(content)
+
+      mentioned_string =
+        Enum.reduce(mentioned_users, "", fn mentioned_user_id, acc ->
+          mentioned_user_roles = get_roles(guild_id, mentioned_user_id)
+
+          acc <>
+            "roles for mentioned user #{mentioned_user_id}, roles=#{inspect(mentioned_user_roles)}\n"
+        end)
+
+      Api.create_message(
+        channel_id,
+        "message has mentions, list of user ids mentioned=#{inspect(mentioned_users)}\n" <>
+          "author roles=#{inspect(author_roles)}\n" <>
+          mentioned_string
+      )
     end
   end
 
-  # # get their roles
-  # def get_roles(user_id) when is_integer(user_id) do
-  #   Nostrum.Api.get_guild_member(guild_id, user_id)
-  # end
-  
+  # get their roles
+  def get_roles(guild_id, user_id) when is_integer(guild_id) and is_integer(user_id) do
+    {:ok, %Nostrum.Struct.Guild.Member{roles: roles}} =
+      Nostrum.Api.get_guild_member(guild_id, user_id)
+
+    roles
+  end
+
   @doc """
   does the passed in string have a discord mention which looks like <@id> where id is a big integer
   """
@@ -39,9 +61,10 @@ defmodule DopaTeam.Consumer do
   @spec get_mentions(String.t()) :: [integer()]
   def get_mentions(message_content) when is_binary(message_content) do
     regex = ~r/<@(\d+)>/
+
     Regex.scan(regex, message_content)
     |> Enum.map(fn [_ | [id]] -> id end)
-    |> Enum.map(fn id_string -> 
+    |> Enum.map(fn id_string ->
       {id, ""} = Integer.parse(id_string)
       id
     end)
