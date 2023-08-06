810f80313c88ee68f467e9c8fe7b4125a84f7d7d
Ellyse <ellyxir>
2023-08-06 18:23:37 +0000

    log audit trail

--- lib/dopa_team/consumer.ex
+++ lib/dopa_team/consumer.ex
@@ -4,11 +4,19 @@ defmodule DopaTeam.Consumer do
   alias Nostrum.Api
   require Logger
 
+  @sdr_guild_id 821_855_117_539_541_003
+  @bot_test_guild_id 964_397_371_213_615_124
+  @logging_channel %{
+    @sdr_guild_id => nil,
+    @bot_test_guild_id => 964_397_371_666_604_076
+  }
+
   @sdr_channel 849_267_950_988_820_482
-  @bot_test_dm_asking 964_397_371_666_604_079
-  @channels [@sdr_channel, @bot_test_dm_asking]
+  @bot_test_dm_asking_channel 964_397_371_666_604_079
+  @channels [@sdr_channel, @bot_test_dm_asking_channel]
   @adult_role_name "18+"
   @minor_role_name "<18"
+  @closed_dm_role_name "Closed DM"
 
   def handle_event(
         {:MESSAGE_CREATE,
@@ -27,14 +35,45 @@ defmodule DopaTeam.Consumer do
 
       if not is_dm_request_allowed?(guild_id, author_roles, mentioned_users) do
         msg =
-          "<@#{author_id}> please note that the user(s) you have mentioned is a minor and requesting to DM a minor is not allowed. See https://discord.com/channels/821855117539541003/928601274352541718/1086113286438789120 for more information.\n"
+          "<@#{author_id}> please note that the user(s) you have mentioned is a minor and/or has the Closed DM role. Your request is not allowed according to server rules. See https://discord.com/channels/821855117539541003/928601274352541718/1086113286438789120 for more information.\n"
 
         Api.create_message(
           channel_id,
           content: msg,
           message_reference: %{message_id: msg_id}
         )
+
+        log_illegal_dm_request(guild_id, author_id, mentioned_users)
       end
+    end
+  end
+
+  @spec log_illegal_dm_request(Nostrum.Struct.Guild.id(), Nostrum.Struct.User.id(), [
+          Nostrum.Struct.User.id()
+        ]) :: any()
+  def log_illegal_dm_request(guild_id, author_id, mentioned_users) do
+    mention_refs =
+      Enum.reduce(mentioned_users, "", fn id, acc ->
+        "#{acc}<@#{id}> (user id: #{id}), "
+      end)
+
+    log_event(
+      guild_id,
+      "Logging - <@#{author_id}> (user id #{author_id}) requested DM to minor/closed DM, users=#{mention_refs}"
+    )
+  end
+
+  @spec log_event(Nostrum.Struct.Guild.id(), String.t()) :: any()
+  def log_event(guild_id, log_message) when is_integer(guild_id) and is_binary(log_message) do
+    case Map.fetch(@logging_channel, guild_id) do
+      {:ok, channel_id} ->
+        Api.create_message(
+          channel_id,
+          log_message
+        )
+
+      :error ->
+        Logger.warning("no channel for logging configured for guild id #{guild_id}")
     end
   end
 
@@ -55,18 +94,20 @@ defmodule DopaTeam.Consumer do
     server_roles = get_server_roles(guild_id)
     {:ok, adult_role_id} = get_role_id_by_name(server_roles, @adult_role_name)
     {:ok, minor_role_id} = get_role_id_by_name(server_roles, @minor_role_name)
+    {:ok, closed_dm_role_id} = get_role_id_by_name(server_roles, @closed_dm_role_name)
 
     is_author_adult = Enum.member?(author_roles, adult_role_id)
     # is_author_minor = Enum.member?(author_roles, minor_role_id)
 
     # is_mentioned_adult = user_has_role?(guild_id, mentioned_user_id, adult_role_id)
     is_mentioned_minor = user_has_role?(guild_id, mentioned_user_id, minor_role_id)
+    is_mentioned_closed_dm = user_has_role?(guild_id, mentioned_user_id, closed_dm_role_id)
     is_mentioned_bot = is_user_bot?(mentioned_user_id)
 
     cond do
+      is_mentioned_closed_dm -> false
       # if we are mentioning a bot, who cares
       is_mentioned_bot -> true
-
       # if author is adult and mentioned user is minor
       is_author_adult && is_mentioned_minor -> false
       # otherwise its all good
